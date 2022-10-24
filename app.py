30595eb27bf21eb0153b21cd07585a227e882643
Brenton <brenton@breakdesign.com.au>
2022-10-24 15:00:41 +1100

    Updated after testing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///inventory.db', echo=True)
+engine = create_engine('sqlite:///inventory.db', echo=False)
 Session = sessionmaker(bind=engine)  # create a session
 session = Session()  # create a session instance
 Base = declarative_base()  # create a base class
@@ -27,16 +27,14 @@ class Item(Base):  # Table name: item
 
 #clean data before adding to database
 def clean_date(date_str):
-    months = ['January', 'February', 'March', 'April', 'May', 'June',
-                'July', 'August', 'September', 'October', 'November', 'December']
-    date_str = date_str.split(' ')
-    month = int(months.index(date_str[0]) + 1)
-    day = int(date_str[1].replace(',', ''))
-    year = int(date_str[2])
+    date_str = date_str.split('/')
+    month = int(date_str[0])
+    day = int(date_str[1])
+    year =  int(date_str[2])
     return datetime.date(year, month, day)
 
-def clean_price(prince_str):
-    return float(prince_str.replace('$', '').replace(',', ''))
+def clean_price(price_str):
+    return float(price_str.replace('$', '').replace(',', ''))
 
 def convert_float_to_int(num):
     return int(num)
@@ -47,20 +45,22 @@ def add_csv():
         data = csv.reader(csvfile)
         for row in data:
             if session.query(Item).filter_by(name=row[0]).one_or_none() is None:
-                item = Item(name=row[0].replace('"', ''), price=convert_float_to_int(clean_price(
+                item = Item(name=row[0], price=convert_float_to_int(clean_price(
                     row[1])), quantity=row[2], date=clean_date(row[3]))
                 session.add(item)
         session.commit()
 
 # add Item to database
-def add_item(name, price, quantity,  date):
-    if type(name) is str and type(price) is int and type(quantity) is int and type(date) is datetime.date:
-        item = Item(name=name, price=convert_float_to_int(
-        price), quantity=quantity, date=date)
+def add_item(name, price, quantity, date):
+    if session.query(Item).filter_by(name=name).one_or_none() is None:
+        new_item_name = name
+        new_item_price = convert_float_to_int(clean_price(price))
+        new_item_date = clean_date(date)
+        item = Item(name=new_item_name, price=new_item_price, quantity=quantity, date=new_item_date)
         session.add(item)
     else:
         print('\rPlease enter valid data')
-        print('For example: Fruitloops, 5, 8, 2022-01-01')
+        print('For example: Fruitloops, 5, 8, 01/01/2022 (MM-DD-YYYY')
     session.commit()
 
 # get all items from database
@@ -70,12 +70,16 @@ def get_all_items():
 
 # get item from database by ID
 def get_item_by_id(id):
-    if type(id) is int:
-        item = session.query(Item).filter_by(id=id).one_or_none()
-        if item is not None:
-            return(item)
-        else:
-            print('\rItem not found')
+    id_options = []
+    for item in session.query(Item):
+        id_options.append(item.id)
+    if int(id) in id_options:
+        item = session.query(Item).filter_by(id=id).one()
+        print('\rID: ' + str(item.id))
+        print('\rName: ' + item.name)
+        print('\rPrice: $' + str(item.price))
+        print('\rQuantity: ' + str(item.quantity))
+        print('\rDate: ' + str(item.date))
     else:
         print('\rPlease enter a valid ID')
 
@@ -89,33 +93,39 @@ def backup_items():
 #show menu
 def show_menu():
     print('\n******** Store Inventory App ********')
-    print('\rPlease select from the following options:')
-    print('\rV: Display an item by its Id')
-    print('\rA: Add an item to the Store inventory')
-    print('\rS: Show all items from the Store inventory')
-    print('\rB: Backup all iventory items to a csv')
-    print('\rQ: Quit the App')
 
     while True:
+        print('\nPlease select from the following options:')
+        print('\rV: Display an item by its Id')
+        print('\rA: Add an item to the Store inventory')
+        print('\rB: Backup all iventory items to a csv')
+        print('\rQ: Quit the App')
+
         user_input = input('\rPlease select an option: ').upper()
-        if user_input in ['V', 'A', 'S', 'B', 'Q']:
-            return user_input
-        else:
-            print('\rPlease select a valid option')
-        if user_input == 'V':
-            get_item_by_id(input('\rPlease enter a valid ID of the item: '))
-        elif user_input == 'A':
-            print('\nPlease enter the details of the new item:')
-            print('\rFor example: Fruitloops, 5, 8, 2022-01-01')
-            add_item(input('\rName: '),
+        if user_input in ['V', 'A', 'B', 'Q']:
+            if user_input == 'V':
+                get_item_by_id(input('\rPlease enter a valid ID of the item: '))
+            elif user_input == 'A':
+                print('\nPlease enter the details of the new item:')
+                print('\rFor example: Fruitloops, 5, 8, 01/01/2022 (MM-DD-YYYY)')
+                add_item(input('\rName: '),
                     input('\rPrice: $'),
                     input('\rQuantity: '),
                     input('\rDate: '))
-        elif user_input == 'S':
-            get_all_items()
-        elif user_input == 'B':
-            backup_items()
-        elif user_input == 'Q':
-            print('\rExiting...')
-            time.sleep(1.5)
-            exit()
+            elif user_input == 'S':
+                get_all_items()
+            elif user_input == 'B':
+                backup_items()
+            elif user_input == 'Q':
+                print('\rExiting...')
+                time.sleep(1.5)
+                exit()
+        else:
+            print('\rPlease select a valid option')
+        
+
+# dunder main
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)  # create table
+    add_csv() # adds all of the items to the table
+    show_menu()
